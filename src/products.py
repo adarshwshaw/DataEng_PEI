b22384b33b33191978e48e7c87dfc046ccabf602
Adarsh Shaw <adarshwshaw@gmail.com>
2025-05-30 14:43:41 +0530

    fixed product

--- src/products.py
+++ src/products.py
@@ -24,25 +24,38 @@ schema = StructType([
     StructField("_corrupt_record", StringType(),True)
 ])
 
-def load_dataset(spark,files,corrupt_path):
+def load_dataset(spark,files):
     df = spark.read.option("header", True).option("mode", "PERMISSIVE")\
         .option("columnNameOfCorruptRecord", "_corrupt_record").csv(files, schema=schema)
     df = df.withColumn("load_ts", current_timestamp())
-    badRecordsDF = df.filter(col("_corrupt_record").isNotNull())
+    return df
 
-    # Save bad records to a CSV file
-    badRecordsDF.write.option("header", "true").mode("append")\
-        .parquet(corrupt_path)
-    
-    goodrecdf = df.filter(col("_corrupt_record").isNull()).drop(col('_corrupt_record'))
-    return goodrecdf
+def load_enriched_data(spark,bronze_df, silver_table):
+    # target_table = DeltaTable.forName(spark, silver_table)
+    # target_table.alias("target").merge(
+    #     bronze_df.alias("source"),on
+    # ).whenMatchedUpdateAll(update_condition)\
+    #     .whenNotMatchedInsertAll().execute()
+    bronze_df.createOrReplaceTempView("bronze_t")
+    query = f"""
+        Merge into {silver_table} tar
+        using bronze_t src 
+        on tar.product_id=src.product_id
+        when matched AND src.load_ts >= tar.load_ts THEN UPDATE SET 
+        tar.category = src.category,
+        tar.sub_category = src.sub_category,
+        tar.load_ts = src.load_ts
+        when not matched then insert (
+        product_id, category, sub_category, load_ts
+        ) VALUES (
+        src.product_id, src.category, src.sub_category, src.load_ts
+        )
+        """
+    print(query)
+    spark.sql(
+        query
+    )
 
-def load_enriched_data(spark,bronze_df, silver_table, on, update_condition):
-    target_table = DeltaTable.forName(spark, silver_table)
-    target_table.alias("target").merge(
-        bronze_df.alias("source"),on
-    ).whenMatchedUpdateAll(update_condition)\
-        .whenNotMatchedInsertAll().execute()
 
 
 def main():
@@ -51,19 +64,19 @@ def main():
             .getOrCreate()
 
         directory = "/FileStore/products"
-        corrupt_path="/FileStore/badrecords/products"
+        # corrupt_path="/FileStore/badrecords/products"
 
         files = getUnprocessedFiles(directory=directory, obj='product',  spark=spark)
         
         filepath_to_process = [f['file_path'] for f in files]
-        df = load_dataset(spark,filepath_to_process,corrupt_path)
+        df = load_dataset(spark,filepath_to_process)
         if (df.isEmpty()):
             print("nothing to load")
             return 
         df.write.mode('append').saveAsTable('bronze.products')
+        df1 = spark.sql("select product_id,category,sub_category,_corrupt_record,load_ts from bronze.products where _corrupt_record is null and load_ts = (select max(load_ts) from bronze.products)").dropDuplicates()
         mark_file_process(spark,files)
-        df = df.select("product_id","category","sub_category","load_ts").dropDuplicates()
-        load_enriched_data(spark,df,"silver.products","target.product_id=source.product_id","source.load_ts > target.load_ts")
+        load_enriched_data(spark,df1,"silver.products_1")
     except Exception as e:
         print(e)
     
---
+++ test/test_product.py
@@ -0,0 +1,56 @@
+import os.path
+
+import pytest
+from pyspark.sql import SparkSession
+from src.products import load_dataset
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+@pytest.fixture(scope="module")
+def spark():
+    spark = SparkSession.builder \
+        .appName("PySpark Pytest Example") \
+        .master("local[*]") \
+        .getOrCreate()
+    print("SparkSession started.")
+    yield spark
+    spark.stop()
+    print("SparkSession stopped.")
+
+def test_corrupt_record(spark):
+    data=[("OFF-LA-10003148","Office Supplies","Labels","Avery 51","Illinois",5.04,"OFF-LA-10003148,Office Supplies,Labels,Avery 51,Illinois,5.04,dfas")]
+    df = load_dataset(spark, [os.path.join(os.path.dirname(__file__), 'resource/test_product.csv')])
+    bdf = df.filter(col("_corrupt_record").isNotNull())
+    res = [tuple(row) for row in bdf.collect()]
+    res = [row[:-1] for row in res]
+    assert res[0]==data[0]
+
+def test_product_bad_count(spark):
+    df = load_dataset(spark,[os.path.join(os.path.dirname(__file__),'resource/test_product.csv')])
+    assert df.count() == 5
+    bdf = df.filter(col("_corrupt_record").isNotNull()).collect()
+    assert len(bdf) == 2
+
+
+def test_product_good_count(spark):
+    df = load_dataset(spark, [os.path.join(os.path.dirname(__file__), 'resource/test_product.csv')])
+    assert df.count() == 5
+    gdf = df.filter(col("_corrupt_record").isNull()).collect()
+    assert len(gdf) == 3
+
+
+def test_product_schema(spark):
+    schema = StructType([
+        StructField("product_id", StringType(), True),
+        StructField("category", StringType(), True),
+        StructField("sub_category", StringType(), True),
+        StructField("product_name", StringType(), True),
+        StructField("state", StringType(), True),
+        StructField("price_per_product", DoubleType(), True),
+        StructField('_corrupt_record', StringType(), True),
+        StructField("load_ts", TimestampType(), False)
+    ])
+    df = spark.createDataFrame([],schema=schema)
+    gdf = load_dataset(spark,[os.path.join(os.path.dirname(__file__),'resource/test_product.csv')])
+    gdf.cache()
+    assert df.schema == gdf.schema
